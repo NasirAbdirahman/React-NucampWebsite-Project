9f0b390bab03f74eecf2be20a4ecd141c5dec126
AbdulNasir Abdirahman <nasir.a.abdirahman@outlook.com>
2021-02-19 18:22:28 -0800

    Combined Reducers

---
+++ nucampsite/src/redux/campsites.js
@@ -0,0 +1,10 @@
+import { CAMPSITES } from '../shared/campsites';
+
+//Reducer Function
+
+export const Campsites = (state = CAMPSITES, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
--- nucampsite/src/redux/configureStore.js
+++ nucampsite/src/redux/configureStore.js
@@ -1,13 +1,20 @@
-import { createStore } from 'redux';
-import { Reducer, initialState } from './reducer';
+import { createStore, combineReducers } from 'redux';
+import { Campsites } from './campsites';
+import { Comments } from './comments';
+import { Partners } from './partners';
+import { Promotions } from './promotions';
 
+//Is the intial State of our App which we get straight from the src data files. Combines All our reducers in object and then gives it to the create store//
 export const ConfigureStore = () => {
     const store = createStore(
-        Reducer,
-        initialState
+        combineReducers({
+            campsites: Campsites,
+            comments: Comments,
+            partners: Partners,
+            promotions: Promotions
+        })
     );
 
     return store;
 }; 
 
-//Function returns redux store//
