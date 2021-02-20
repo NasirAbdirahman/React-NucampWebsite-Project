bef419abcbb5ad6ef8434eb70fdcb0c75cdde55b
AbdulNasir Abdirahman <nasir.a.abdirahman@outlook.com>
2021-02-20 13:09:45 -0800

    Assignment

--- nucampsite/src/components/AboutComponent.js
+++ nucampsite/src/components/AboutComponent.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { baseUrl } from '../shared/baseUrl';
+import { Loading } from './LoadingComponent';
+
 
 
 function About(props) {
-
-    const partners = props.partners.map(partner => {
-        return (
-            <Media tag="li" key={partner.id}>
-                <RenderPartner partner={partner} />
-            </Media>
-        );
-    });
 
     return (
         <div className="container">
@@ -65,11 +60,7 @@ function About(props) {
                 <div className="col-12">
                     <h3>Community Partners</h3>
                 </div>
-                <div className="col mt-4">
-                    <Media list>
-                        {partners}
-                    </Media>
-                </div>
+                <PartnerList  partners={props.partners}/>
             </div>
         </div>
     );
@@ -79,7 +70,7 @@ function RenderPartner ({partner}) {
     if (partner) {
         return (
            <React.Fragment>
-               <Media object src={partner.image} alt={partner.name} width="150"/>
+               <Media object src={baseUrl + partner.image} alt={partner.name} width="150"/>
                <Media body className="ml-5 mb-4">
                    <Media heading> 
                         {partner.name} 
@@ -90,6 +81,45 @@ function RenderPartner ({partner}) {
         );
     } 
     return <div />;
+
+}
+
+function PartnerList (props) {
+
+    const partners = props.partners.partners.map(partner => {
+        return (
+            <Media tag="li" key={partner.id}>
+                <RenderPartner partner={partner} />
+            </Media>
+        );
+    });
+    if (props.partners.isLoading) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    if (props.partners.errMess) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <h4>{props.partners.errMess}</h4>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+    return (
+        <div className="col mt-4">
+            <Media list/>
+                {partners}
+            <Media/>
+        </div>
+    )
 
 }
 
