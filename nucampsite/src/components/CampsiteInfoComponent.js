6836332bd6afbedca15cbbbd170e051101e8eb8c
AbdulNasir Abdirahman <nasir.a.abdirahman@outlook.com>
2021-02-19 22:47:02 -0800

    fetch Post comment example:Added comment to JSON server

--- nucampsite/src/components/CampsiteInfoComponent.js
+++ nucampsite/src/components/CampsiteInfoComponent.js
@@ -35,7 +35,7 @@ class CommentForm extends Component {
 
     handleSubmit(values) {  
         this.toggleModal();
-        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text)
+        this.props.postComment(this.props.campsiteId, values.rating, values.author, values.text)
     }
 
 
@@ -112,7 +112,7 @@ function RenderCampsite({campsite}) {
     );
 }
 
-function RenderComments({comments, addComment, campsiteId}) {
+function RenderComments({comments, postComment, campsiteId}) {
     if (comments) {
         return (
             <div className="col-md-5 m-1">
@@ -127,7 +127,7 @@ function RenderComments({comments, addComment, campsiteId}) {
                         </div>
                     );
                 })}
-                <CommentForm campsiteId={campsiteId} addComment={addComment}/>
+                <CommentForm campsiteId={campsiteId} postComment={postComment}/>
             
             </div>
         );
@@ -175,9 +175,9 @@ function CampsiteInfo(props) {
                 <div className="row">
                     <RenderCampsite campsite={props.campsite} />
                     <RenderComments 
-                    comments={props.comments}
-                    addComment={props.addComment}
-                    campsiteId={props.campsite.id} />
+                        comments={props.comments}
+                        postComment={props.postComment}
+                        campsiteId={props.campsite.id} />
                 </div>
             </div>
         );
